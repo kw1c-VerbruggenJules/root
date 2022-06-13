13d7c4a74721c9f360de73ad6dbf1cd5f94295c6
Jules <Jules.Verbruggen@edu-kw1c.nl>
2022-06-13 22:27:10 +0200

    13-06-2022

--- includes/foreground.php
+++ includes/foreground.php
@@ -7,14 +7,3 @@
 //header
 include 'includes/header.php';
 
-//checks if the page URL contains the startup=1. if so it does play the gameboy intro but if it doesn't it will
-$startUp = $_GET['startUp'];
-  if (empty($startUp)) 
-  {
-    echo '<div><img id="foreground" src="../images/foreground.png" alt="foreground"><img id="battery" src="../images/BATTERY.png" alt="battery"><img id="logo" src="../images/techbrothers_logo.png" alt="logo"><audio id="gameboyAudio"><source src="../sound/gameboy-sound.mp3" type="audio/mpeg"></audio><div id="fakeBackground"></div></div>';
-  } 
-  else 
-  {
-    echo '<div><img id="foreground" src="../images/foreground.png" alt="foreground"><img id="battery" src="../images/BATTERY.png" alt="battery"></div>';
-  }
-?>
--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 /**
  * User: J. Verbruggen
  * Date: 15/06/2022
@@ -6,20 +6,53 @@
  */
 //header
 include '../includes/header.php';
-?>
-<!--The company logo linking to the homepage-->
-<nav>
-    <div class="container nav">
-        <div class="row justify-content-center nav-items">
-            <div class="col-md-4 nav-item">
-                Row 1 Col 1
-            </div>
-            <div class="col-md-4 nav-item">
-                Row 1 Col 2
-            </div>
-            <div class="col-md-4 nav-item">
-                Row 1 Col 3
-            </div>
+?> -->
+<nav class="navbar navbar-expand-sm navbar-light" style="background-color:gray">
+    <div class="container">
+        <a href="#" class="navbar-brand mb-0 h1">
+            <img class="d-inline-block align-top" src="https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png" width="30" height="30" />
+            Navbar
+        </a>
+        <!-- Hamburger button -->
+        <button type="button" data-bs-toggle="colapse" data-bs-target="#navbarNav" class="navbar-toggler" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- NAV items -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a href="#" class="nav-link active">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Features
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a href="#" class="dropdown-item">
+                                Feature #1
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item">
+                                Feature #2
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item">
+                                Feature #3
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item active">
+                    <a href="#" class="nav-link">
+                        Pricing
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
